c6727543bd6ade16f14694f8e79cf1608189cd13
kajumz <57329943+kajumz@users.noreply.github.com>
2022-12-29 21:13:18 +0300

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,1 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n=len(nums)
+        d={}
+        l=[]
+        for i in range(n):
+            ntf= target-nums[i]
+            if ntf in d.keys():
+                l.append(d[ntf])
+                l.append(i)
 
+            d[nums[i]] = i
+        return l 
