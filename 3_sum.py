13718a08da3ff3d82991f06604b5c87cc34e91d3
kajumz <57329943+kajumz@users.noreply.github.com>
2023-01-04 18:03:48 +0300

    Create 3_sum.py

---
+++ 3_sum.py
@@ -0,0 +1,29 @@
+# 1) sort 2) use 2 pointer to check sum == 0 
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res=[]
+        nums.sort()
+        for i in xrange(len(nums) - 2):
+            if nums[i] == nums[i-1] and i>0:
+                continue
+            l,r=i+1, len(nums) - 1
+            while l<r:
+                s=nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l+=1
+                elif s>0:
+                    r-=1
+                else:
+                    res.append((nums[i],nums[l],nums[r]))
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1 
+
+        return res
