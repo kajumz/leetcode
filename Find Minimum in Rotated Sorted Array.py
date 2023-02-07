dd19f463e05bc1c1b1ca90b82b4ef3ec5ef86719
kajumz <57329943+kajumz@users.noreply.github.com>
2023-02-07 14:32:50 +0300

    Create Find Minimum in Rotated Sorted Array.py

---
+++ Find Minimum in Rotated Sorted Array.py
@@ -0,0 +1,12 @@
+#using binary search
+def findMin(self, nums: List[int]) -> int:
+        start,end = 0, len(nums)-1
+        while start<end:
+            if nums[start]<nums[end]:
+                return nums[start]
+            mid = (start+end)//2
+            if nums[mid]<nums[end]:
+                end = mid
+            else:
+                start = mid + 1
+        return nums[start]
