50caca025f9fd0ff489315c7980c1685d3c2eebe
kajumz <57329943+kajumz@users.noreply.github.com>
2023-02-16 21:04:20 +0300

    Create Koko Eating Bananas.py

---
+++ Koko Eating Bananas.py
@@ -0,0 +1,15 @@
+#binary search on answer
+def minEatingSpeed(self, piles, h):
+        """
+        :type piles: List[int]
+        :type h: int
+        :rtype: int
+        """
+        l, r = 1, max(piles)
+        while l < r:
+            m = (l + r) / 2
+            if sum((p + m - 1) / m for p in piles) > h:
+                l = m + 1
+            else:
+                r = m
+        return l
