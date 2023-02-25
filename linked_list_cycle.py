0287141de09532b26961c4bf4d47d648b1b09c3a
kajumz <57329943+kajumz@users.noreply.github.com>
2023-02-26 00:36:04 +0300

    Create linked_list_cycle.py

---
+++ linked_list_cycle.py
@@ -0,0 +1,12 @@
+#if fast not exist, then false
+def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        slow = head
+        fast = head.next
+        while slow!=fast:
+            if not fast or not fast.next:
+                return False
+            slow=slow.next
+            fast=fast.next.next
+        return True
