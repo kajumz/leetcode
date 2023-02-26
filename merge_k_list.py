0ff839d921ed22327a736912127b3479a5779f89
kajumz <57329943+kajumz@users.noreply.github.com>
2023-02-26 23:00:24 +0300

    Create merge_k_list.py

---
+++ merge_k_list.py
@@ -0,0 +1,34 @@
+#merge two than merge another, in other words merge recursive
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty())
+        {
+            return nullptr;
+        }
+        while(lists.size() > 1)
+        {
+            lists.push_back(mergetwolists(lists[0],lists[1]));
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
+        }
+        return lists.front();
+    }
+    
+    ListNode* mergetwolists(ListNode* l1, ListNode* l2)
+    {
+        if(l1==nullptr) return l2;
+        if(l2==nullptr) return l1;
+        if(l1->val <= l2->val)
+        {
+            l1->next = mergetwolists(l1->next,l2);
+            return l1;
+        }
+        else
+        {
+            l2->next = mergetwolists(l1,l2->next);
+            return l2;
+        }
+            
+    }
+};
