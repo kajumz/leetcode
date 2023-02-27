3682ed25373953c85d222b2957a995e4cbe7eba9
kajumz <57329943+kajumz@users.noreply.github.com>
2023-02-27 23:01:03 +0300

    Create max depth of binary tree.py

---
+++ max depth of binary tree.py
@@ -0,0 +1,6 @@
+def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left,right)+1
